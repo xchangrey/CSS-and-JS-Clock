f48c6c22efcfb936d61134fbdbd6220ccc3d993f
xchangrey <geroychristian@gmail.com>
2018-07-11 08:03:36 +0800

    Finishing up the CSS and JS Clock proj on #javasScript30 challenge

--- clock.js
+++ clock.js
@@ -1,21 +1,26 @@
 const secondHand = document.querySelector(".second-hand");
 const minsHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector("hour-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate(){
 	const now = new Date();
-	const seconds = now.getSecond();
+	const seconds = now.getSeconds();
 	const secondsDegree = ((seconds / 60) * 360) + 90;
-	secondHand.style.transform = `rotate(${secondsDegree})deg`;
+	secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
 
 	const minutes = now.getMinutes();
-	const minutesDegree = ((minutes / 60) * 360) + 90;
-	minsHand.style.transform = `rotate(${minutesDegree})deg`;
+	const minsDegree = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+	minsHand.style.transform = `rotate(${minsDegree}deg)`;
+	console.log(minutes);
 
 	const hour = now.getMinutes();
-	const hourDegree == ((minutes / 12) * 360) + 90;
-	hourHand.style.transform = `rotate(${hourDegree})deg`;
+	const hourDegree = ((minutes / 12) * 360) + ((minutes/60)*30) + 90;
+	hourHand.style.transform = `rotate(${hourDegree}deg)`;
 
 }
 
 setInterval(setDate, 1000);
+
+setDate();
+
